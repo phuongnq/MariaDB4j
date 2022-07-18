fa584d8a7eb5a2af0140262451fe9c045a2794e0
Phil Nguyen <nquangphuong@gmail.com>
2022-07-18 20:26:22 +0700

    Add debug log for aborted connection (#91)

--- mariaDB4j-core/src/main/java/ch/vorburger/mariadb4j/MariaDBOutputStreamLogDispatcher.java
+++ mariaDB4j-core/src/main/java/ch/vorburger/mariadb4j/MariaDBOutputStreamLogDispatcher.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -25,7 +25,7 @@ import org.slf4j.event.Level;
 
 /**
  * OutputStreamLogDispatcher for MariaDB. See <a href="https://github.com/vorburger/MariaDB4j/issues/27">issue #27</a>.
- * 
+ *
  * @author Michael Vorburger
  */
 public class MariaDBOutputStreamLogDispatcher extends OutputStreamLogDispatcher {
@@ -37,6 +37,8 @@ public class MariaDBOutputStreamLogDispatcher extends OutputStreamLogDispatcher 
         else { // STDERR
             if (line.contains("ERROR") || line.contains("error"))
                 return Level.ERROR;
+            else if (line.toLowerCase().contains("warning") && line.toLowerCase().contains("aborted connection"))
+               return Level.DEBUG;
             else
                 return Level.INFO;
         }
