ce9b16139ac97ef606639af99023be3d176da9fb
Phil Nguyen <nquangphuong@gmail.com>
2024-10-08 17:26:27 +0700

    Add binaries for MariaDB 11.4.3 MacOS

--- DBs/mariaDB4j-db-mac64-11.4.3/src/main/resources/ch/vorburger/mariadb4j/mariadb-11.4.3/osx/share/mariadb_sys_schema.sql
+++ DBs/mariaDB4j-db-mac64-11.4.3/src/main/resources/ch/vorburger/mariadb4j/mariadb-11.4.3/osx/share/mariadb_sys_schema.sql
@@ -17,7 +17,12 @@ SET NAMES utf8;
 SET @sql_log_bin = @@sql_log_bin;
 SET sql_log_bin = 0;
 
-CREATE DATABASE IF NOT EXISTS sys DEFAULT CHARACTER SET utf8;
+CREATE DATABASE IF NOT EXISTS sys DEFAULT CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci;
+
+-- If the database had existed, let's recreate its db.opt:
+-- * to fix it if it contained unexpected charset/collation values
+-- * to create it if it was removed in a mistake
+ALTER DATABASE sys CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci;
 
 USE sys;
 
@@ -56,8 +61,9 @@ VIEW version (
   sys_version,
   mysql_version
 ) AS 
-SELECT '1.5.1' AS sys_version, 
+SELECT '1.5.2' AS sys_version,
         version() AS mysql_version;
+
 -- Copyright (c) 2014, 2015, Oracle and/or its affiliates. All rights reserved.
 --
 -- This program is free software; you can redistribute it and/or modify
@@ -2894,6 +2900,84 @@ VIEW schema_redundant_indexes (
         AND dominant_keys.non_unique = 0
       )
     );
+
+--
+-- View: privileges_by_table_by_level
+--
+-- Shows granted privileges broken down by the table on which they allow access
+-- and the level on which they were granted:
+-- - user_privileges
+-- - schema_privileges
+-- - table_privileges
+--
+-- mysql> select * from sys.privileges_by_table_by_level;
+-- +--------------+------------+--------------------+----------------+--------+
+-- | TABLE_SCHEMA | TABLE_NAME | GRANTEE            | PRIVILEGE_TYPE | LEVEL  |
+-- +--------------+------------+--------------------+----------------+--------+
+-- | test         | v1         | 'oleg'@'localhost' | SELECT         | GLOBAL |
+-- | test         | t1         | 'oleg'@'localhost' | SELECT         | GLOBAL |
+-- | test         | v1         | 'oleg'@'localhost' | INSERT         | GLOBAL |
+-- | test         | t1         | 'oleg'@'localhost' | INSERT         | GLOBAL |
+-- | test         | v1         | 'oleg'@'localhost' | UPDATE         | GLOBAL |
+-- | test         | v1         | 'PUBLIC'@''        | SELECT         | SCHEMA |
+-- | test         | t1         | 'PUBLIC'@''        | SELECT         | SCHEMA |
+-- | test         | v1         | 'PUBLIC'@''        | INSERT         | SCHEMA |
+-- | test         | t1         | 'PUBLIC'@''        | INSERT         | SCHEMA |
+-- | test         | v1         | 'PUBLIC'@''        | UPDATE         | SCHEMA |
+-- | test         | t1         | 'PUBLIC'@''        | UPDATE         | SCHEMA |
+-- | test         | v1         | 'PUBLIC'@''        | DELETE HISTORY | SCHEMA |
+-- | test         | t1         | 'PUBLIC'@''        | DELETE HISTORY | SCHEMA |
+-- | test         | t1         | 'oleg'@'%'         | SELECT         | TABLE  |
+-- | test         | t1         | 'oleg'@'%'         | UPDATE         | TABLE  |
+-- | test         | v1         | 'oleg'@'%'         | SELECT         | TABLE  |
+-- +--------------+------------+--------------------+----------------+--------+
+
+CREATE OR REPLACE
+  ALGORITHM = TEMPTABLE
+  DEFINER = 'mariadb.sys'@'localhost'
+  SQL SECURITY INVOKER
+VIEW privileges_by_table_by_level (
+  TABLE_SCHEMA,
+  TABLE_NAME,
+  GRANTEE,
+  PRIVILEGE,
+  LEVEL
+) AS
+SELECT t.TABLE_SCHEMA,
+       t.TABLE_NAME,
+       privs.GRANTEE,
+       privs.PRIVILEGE_TYPE,
+       privs.LEVEL
+FROM INFORMATION_SCHEMA.TABLES AS t
+JOIN ( SELECT NULL AS TABLE_SCHEMA,
+              NULL AS TABLE_NAME,
+              GRANTEE,
+              PRIVILEGE_TYPE,
+             'GLOBAL' LEVEL
+           FROM INFORMATION_SCHEMA.USER_PRIVILEGES
+         UNION
+       SELECT TABLE_SCHEMA,
+              NULL AS TABLE_NAME,
+              GRANTEE,
+              PRIVILEGE_TYPE,
+              'SCHEMA' LEVEL
+           FROM INFORMATION_SCHEMA.SCHEMA_PRIVILEGES
+         UNION
+       SELECT TABLE_SCHEMA,
+              TABLE_NAME,
+              GRANTEE,
+              PRIVILEGE_TYPE,
+              'TABLE' LEVEL
+           FROM INFORMATION_SCHEMA.TABLE_PRIVILEGES
+       ) privs
+    ON (t.TABLE_SCHEMA = privs.TABLE_SCHEMA OR privs.TABLE_SCHEMA IS NULL)
+   AND (t.TABLE_NAME = privs.TABLE_NAME OR privs.TABLE_NAME IS NULL)
+   AND privs.PRIVILEGE_TYPE IN ('SELECT', 'INSERT', 'UPDATE', 'DELETE',
+                                'CREATE', 'ALTER', 'DROP', 'INDEX',
+                                'REFERENCES', 'TRIGGER', 'GRANT OPTION',
+                                'SHOW VIEW', 'DELETE HISTORY')
+WHERE t.TABLE_SCHEMA NOT IN ('sys', 'mysql','information_schema',
+                             'performance_schema');
 
 -- Copyright (c) 2014, 2015, Oracle and/or its affiliates. All rights reserved.
 --
